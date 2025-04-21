fd5102130abb9ecae9191007e088eff3d549954f
vzelenkova <95612348+vzelenkova@users.noreply.github.com>
2025-04-21 15:46:37 +0300

    Update OperationsTest.java

--- src/test/java/ru/netology/card/test/OperationsTest.java
+++ src/test/java/ru/netology/card/test/OperationsTest.java
@@ -68,7 +68,6 @@ public class OperationsTest {
         var amount = generateInvalidAmount(personalAccountPage.getCardBalance(0));
         var transferPage = personalAccountPage.selectCardToTransfer(secondCardInfo);
         loginPage = transferPage.makeValidTransfer(String.valueOf(amount), firstCardInfo);
-        transferPage.verifyErrorNotification("Ошибка", true);
         
         var actualBalanceFirstCard = personalAccountPage.getCardBalance(0);
         var actualBalanceSecondCard = personalAccountPage.getCardBalance(1);
